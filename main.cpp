5dd237b33aa909c0a5bbd59b6fecbb6eda6fbaad
nixd <nick@robot.lan>
2017-01-29 14:35:56 +0000

    Added a loop to check stuff is working.

--- main.cpp
+++ main.cpp
@@ -8,11 +8,16 @@ int main(int argc, char *argv[])
 {
 	Chip8 c;
 	
-	cout << "loading rom..." << endl;
+	cout << "Loading rom..." << endl;
 	c.loadRom(argv[1]);
 	c.printMemory();
- 	c.runSingleInstruction();
-	c.printMemory();	
+
+	cout << "Beginning execution." << endl;
+	while(true)
+	{
+ 		c.runSingleInstruction();
+	}
+	//c.printMemory();	
 
 	return 0;
 }
