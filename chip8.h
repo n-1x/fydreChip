507cb7f45c1dc94709775f45ac544309ad1884e7
nixd <nick@robot.lan>
2017-01-29 14:33:42 +0000

    fixed some incorret variable declarations

--- chip8.h
+++ chip8.h
@@ -4,24 +4,27 @@ class Chip8
 		typedef unsigned char byte;
 		typedef unsigned short twoByte;
 
-		static const int MEMSIZE = 4096;
-		static const int STACKSIZE = 16;
-		static const int NUM_REGS = 16;
+		static const int MEMSIZE = 0x1000; //4kb of memory
+		static const int STACKSIZE = 16;   //16 stack levels 
+		static const int NUM_REGS = 16;    //16 GP registers
 
 		static const twoByte PROG_START_ADDR = 0x200;
 
 		byte memory[MEMSIZE];
 
-		twoByte registers[NUM_REGS];  //general purpose registers
-		twoByte ip;          //instruction pointer
-		twoByte I;           //Holds an instruction
-		byte sp,             //stack pointer
-		     st,             //sound timer (dec at 64hz)
-		     dt;             //delay timer (dec at 60hz)
-		twoByte stack[STACKSIZE];
+		byte V[NUM_REGS];         //general purpose registers, V[F] is used for flags
+		twoByte ip;               //instruction pointer
+		twoByte I;                //index register
+		byte sp,                  //stack pointer
+		     st,                  //sound timer (dec at 64hz)
+		     dt;                  //delay timer (dec at 60hz)
+		twoByte stack[STACKSIZE]; //the stack
+
+		bool keys[16];            //holds the state of every key on the keyboard
 		
 		//initialise all internal storage components
 		void memInit();
+		void loadFont();
 
 		//decode and run an instruction
 		void execute(const twoByte &b);
