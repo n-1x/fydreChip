92b36c0129bf25c1fe6c0d9db784365cdb76a483
nixd <nick@robot.lan>
2017-02-03 14:41:48 +0000

    Added header comment explaining usage. Properly defined setKeys().

--- chip8.h
+++ chip8.h
@@ -1,3 +1,23 @@
+//File: chip8.h
+//Author: Nicholas J D Dean
+//Date Created: 2017-01-27
+//------------------------------------
+//This will emulate all actions 
+//of the chip 8. Howver to be used it will require some
+//functions to be called externally to deal with things
+//such as timing, keyboard and display.
+//
+//Firstly, you must call decTimers() at 60Hz.
+//
+//Secondly, you must call getDisplay() when drawFlag() == true 
+//to retrieve the graphical output.
+//
+//Finally, you have to send it an array of booleans that tracks
+//the state of the 16 keys that should be attached.
+//
+//Once these systems are in place, loadRom() then call
+//runSingleCycle() as often as possible.
+
 class Chip8
 {
 	private:
@@ -9,14 +29,14 @@ class Chip8
 		static const int NUM_REGS = 16;    //16 GP registers
 
 		static const int DISP_WIDTH = 64,
-	                         DISP_HEIGHT = 32;
+	                    DISP_HEIGHT = 32;
 
 		static const twoByte PROG_START_ADDR = 0x200;
 
 		byte memory[MEMSIZE];
 
 		byte V[NUM_REGS];         //general purpose registers, V[F] is used for flags
-		twoByte ip;               //instruction pointer
+		twoByte pc;               //instruction pointer
 		twoByte I;                //index register
 		byte sp,                  //stack pointer
 		     st,                  //sound timer (dec at 64hz)
@@ -36,17 +56,21 @@ class Chip8
 	public:
 		Chip8();
 
+		//functions for writing stuff to stdout
+		//only used for debugging
+
 		void printStack();
 		void printRegs();
 		void printMainMemory();
 		void printAllMemory();	
 		void printDisplay();
+
 		void loadRom(const char *filePath);
 		void runSingleCycle(); //1 fetch-decode-execute cycle
 		void runInstruction(const twoByte &instruction);  
 
 		void decTimers(); //decrement the delay and sound timers
-		bool drawFlag();
-		void setKeys();
+		bool drawFlag();  //does the screen need to be redrawn
+		void setKeys(bool keyStates[16]); //assign the state of all keys
 		bool *getDisplay();
 };
